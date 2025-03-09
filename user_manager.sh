306ea1b89d2e433e3ea129533ba2e96e17b64a6d
hisham <hisham.elkenany2000@gmail.com>
2025-03-09 21:49:13 +0200

    added shell file to project version 1

---
+++ user_manager.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+
+#Function for adding new users
+add_user()
+{
+	#Assignm username
+	echo "Please enter username you want to add"
+	read username
+
+	#check if username entered already exists
+	if id "$username" &>/dev/null; then
+               echo "Username '$username' exists..Failed To create username '$username' "
+	else
+	#Assign user id
+	echo "Please enter user id"
+	read userid
+
+	sudo useradd -u "$userid" -m -d "/home/$username" -s /bin/bash "$username"
+ 
+	#Assign user password
+	echo "Do you want user to have password ?"
+
+	read option
+
+        if [[ "$option" == "y" || "$option" == "Y" ]]; then
+                sudo passwd "$username"
+        elif [[ "$option" == "n" || "$option" == "N" ]]; then
+                echo "User is created successfully without password"
+        else
+        echo "Invalid Option"
+        fi
+fi 
+}
+
+#Function for deleting existing users
+delete_user()
+{
+	echo "Please enter username of the user you want to delete"
+	read username
+
+	#Option: Delete the user's home directory
+	echo "Do you want to also delete the user's home directory: y/Y for Yes, n/N for No"
+	read option
+
+	if [[ "$option" == "y" || "$option" == "Y" ]]; then
+		sudo userdel -r "$username"		#delete also the home directory	
+	elif [[ "$option" == "n" || "$option" == "N" ]]; then
+		sudo userdel "$username"
+		echo "User is deleted successfully but home directory is kept"
+	else
+	echo "Invalid Option"
+	fi
+}
+
+#Function for listing system Users
+list_users()
+{
+	echo "Listing all registered users:"
+	cut -d: -f1 /etc/passwd		#extract only the username field
+}
+
+#Function for resetting passwords
+reset_password()
+{
+	echo "Please enter username to reset the password:"
+	read username
+
+	if id "$username" &>/dev/null; then		#check if user exists
+    		sudo passwd "$username"
+	else
+    		echo "Username '$username' does not exist"
+fi
+
+}
+
+#Function for checking if a user exists
+check_user()
+{
+		echo "Please enter username:"
+		read username
+	        if id "$username" &>/dev/null; then             #check if user exists
+               		echo "Username '$username' exists"
+       		else
+                	echo "Username '$username'  does not exist"
+fi
+}
+
+
+
+# ************************************************************ Main Menu ***************************************************************
+
+while [ 1 ]; do
+        echo "Choose from the following options"
+        echo "A -> Add User, D -> Delete User, L -> List Users, R -> Reset User's Password, C -> Check User exists or not, E -> Exit:"
+        read cmd
+        if [ "$cmd" == "A" ]; then 
+        add_user
+        elif [ "$cmd" == "D" ]; then
+        delete_user
+        elif [ "$cmd" == "L" ]; then
+        list_users
+        elif [ "$cmd" == "R" ]; then
+        reset_password
+	elif [ "$cmd" == "C" ]; then
+        check_user
+        elif [ "$cmd" == "E" ]; then
+        exit
+        else
+        echo "Invalid Option" 
+        fi
+        done
